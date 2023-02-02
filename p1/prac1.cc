5cabc1bc20394737332e16d9effee388eaa8e41a
gpr11-ua <grp11@alu.ua.es>
2023-02-02 17:47:53 +0100

    inicio de practica

--- p1/prac1.cc
+++ p1/prac1.cc
@@ -1,4 +1,99 @@
-// Programación 2 - Práctica 1
-// DNI:
-// Nombre:
+#include <iostream>
 
+using namespace std;
+
+const int KNAME=40;
+const int KMAXOBSTACLES=20;
+
+enum Error{
+    ERR_OPTION,
+    ERR_DIFFICULTY,
+    ERR_LEVEL,
+    ERR_COORDINATE,
+    ERR_OBSTACLES,
+    ERR_ID,
+    ERR_INSTRUCTION
+};
+
+// Registro para las coordenadas
+struct Coordinate{
+    int row;
+    int column;
+};
+
+// Registro para el jugador
+struct Player{
+    char name[KNAME];
+    int difficulty;
+    int score;
+    int wins;
+    int losses;
+};
+
+// Registro para el nivel
+struct Level{
+    int id;
+    int size;
+    int numObstacles;
+    Coordinate obstacles[KMAXOBSTACLES];
+    Coordinate start;
+    Coordinate finish;
+};
+
+// Función que muestra los mensajes de error
+void error(Error e){
+    switch(e){
+        case ERR_OPTION: cout << "ERROR: wrong option" << endl;
+            break;
+        case ERR_DIFFICULTY: cout << "ERROR: wrong difficulty" << endl;
+            break;
+        case ERR_LEVEL: cout << "ERROR: cannot create level" << endl;
+            break;
+        case ERR_COORDINATE: cout << "ERROR: wrong coordinate" << endl;
+            break;
+        case ERR_OBSTACLES: cout << "ERROR: wrong number of obstacles" << endl;
+            break;
+        case ERR_ID: cout << "ERROR: wrong id" << endl;
+            break;
+        case ERR_INSTRUCTION: cout << "ERROR: wrong instruction" << endl;
+            break;
+    }
+}
+
+// Función que muestra el menú de opciones
+void showMenu(){
+    cout << "[Options]" << endl
+        << "1- Create level" << endl
+        << "2- Delete level" << endl
+        << "3- Show levels" << endl
+        << "4- Play" << endl
+        << "5- Report" << endl
+        << "q- Quit" << endl
+        << "Option: ";
+}
+
+// Función principal (tendrás que añadirle más código tuyo)
+int main(){
+    char option;
+    
+    do{
+        showMenu();
+        cin >> option;
+        cin.get(); // Para evitar que el salto de línea se quede en el buffer de teclado y luego pueda dar problemas si usas "getline"
+        
+        switch(option){
+            case '1': // Llamar a la función para crear un nuevo nivel
+                break;
+            case '2': // Llamar a la función para borrar un nivel existente
+                break;
+            case '3': // Llamar a la función para mostrar los niveles creados
+                break;
+            case '4': // Llamar a la función para jugar
+                break;
+            case '5': // Llamar a la función para mostrar información del jugador
+                break;
+            case 'q': break;
+            default: error(ERR_OPTION); // Muestra "ERROR: wrong option"
+        }
+    }while(option!='q');
+}
